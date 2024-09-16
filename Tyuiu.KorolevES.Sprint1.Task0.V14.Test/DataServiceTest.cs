5b0f26260e428ba1e9fb2d8905daa2d0982cead1
Егор Королев <Tyuiu.KorolevES@outlook.com>
2024-09-16 12:17:36 +0500

    Add project files.

---
+++ Tyuiu.KorolevES.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.KorolevES.Sprint1.Task0.V14.Lib;
+
+namespace Tyuiu.KorolevES.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+        }
+    }
+}
